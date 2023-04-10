e054f880598028f3e7ba1df98582e8b89acc394e
chrismdks <chris.mdks@gmail.com>
2023-04-10 21:32:58 +0300

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import logo192 from "./images/logo192.png";
+
+function Header() {
+    return (
+        <div className="py-2 pl-2" style={{borderBottom: "1px solid #777"}}>
+            <img src={logo192} alt="logo" style={{height:"35px",verticalAlign:"top"}}/>
+            <span className="h2 pt-4 m-2 text-white-50">WatchOPedia</span>
+        </div>
+    );
+}
+export default Header;
