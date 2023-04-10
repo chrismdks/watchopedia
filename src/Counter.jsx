ed34684a0e6fb2e8cc748d59c18d57617c607809
chrismdks <chris.mdks@gmail.com>
2023-04-10 22:29:01 +0300

    usage of hook useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react"; // using Hook named useState
+
 const Counter = () => {
+
+    const [counter, setCounter] = useState(10); //setCounter: to update this particular value (counter)
+
     return (
         <div className="col-12 col-md-4 offset-md-4 border text-white">
             <span className="h2 pt-4 m-2 text-white-50">Fun Counter</span>
@@ -7,7 +12,7 @@ const Counter = () => {
             <button className="btn btn-danger m-1">-1</button>
             <br/>
             <span className="h4">
-                Counter: &nbsp; <span className="text-warning">0</span>
+                Counter: &nbsp; <span className="text-warning">{counter}</span>
             </span>
         </div>
     )
