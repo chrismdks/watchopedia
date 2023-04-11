0896ad539989e5cc74c53da634968d162518438a
chrismdks <chris.mdks@gmail.com>
2023-04-11 07:59:17 +0300

    increment & decrement Counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,14 +2,26 @@ import { useState } from "react"; // using Hook named useState
 
 const Counter = () => {
 
-    const [counter, setCounter] = useState(10); //setCounter: to update this particular value (counter)
+    //setCounter: to update this particular value (counter)
+    const [counter, setCounter] = useState(10);
+    
+    function incrementCounter() {
+        setCounter((prevState) => {
+            return prevState+1
+        })
+    }
+    function decrementCounter() {
+        setCounter((prevState) => {
+            return prevState-1
+        })
+    }
 
     return (
         <div className="col-12 col-md-4 offset-md-4 border text-white">
             <span className="h2 pt-4 m-2 text-white-50">Fun Counter</span>
             <br/>
-            <button className="btn btn-success m-1">+1</button>
-            <button className="btn btn-danger m-1">-1</button>
+            <button className="btn btn-success m-1" onClick={incrementCounter}>+1</button>
+            <button className="btn btn-danger m-1" onClick={decrementCounter}>-1</button>
             <br/>
             <span className="h4">
                 Counter: &nbsp; <span className="text-warning">{counter}</span>
